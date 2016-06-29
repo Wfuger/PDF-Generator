39a84b1b3e08bc76bef077857778e7de79a4d9e0
Wfuger <wfuger@gmail.com>
2016-06-29 12:32:49 -0600

    Initial Commit

---
+++ EMG-PDF/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  EMG-PDF
+//
+//  Created by Will Fuger on 6/29/16.
+//  Copyright © 2016 boogiesquad. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
--- EMG-PDF/ViewController.swift
+++ EMG-PDF/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var storeNumTextField: UITextField!
+    
+    @IBOutlet weak var storeNameTextField: UITextField!
+    
+    @IBOutlet weak var projectNamePicker: UIPickerView!
+    
+    var pickerData = [String]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        storeNameTextField.layer.borderColor = UIColor.blackColor().CGColor
+        storeNumTextField.layer.borderColor = UIColor.blackColor().CGColor
+        
+        self.projectNamePicker.dataSource = self
+        self.projectNamePicker.delegate = self
+        
+        pickerData = ["90% Walk", "Pre-bid Walk", "Pre-Con Walk", "Weekly Visit", "Punch Walk"]
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 
-
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerData[row])
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+    }
+    
 }
 
